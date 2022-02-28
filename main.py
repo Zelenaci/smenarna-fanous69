d114dcbba78cd988047b56ba3d4276dd9ebba6e6
fanous69 <fanyvybiral@seznam.cz>
2022-02-28 13:41:31 +0100

    f

---
+++ main.py
@@ -0,0 +1,25 @@
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="KONEC", fg="red", command=master.destroy)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Pozdrav!", command=self.rekni_ahoj)
+        self.hi_there.pack(side=LEFT)
+
+    def rekni_ahoj(self):
+
+        print ("Ahoj všici!")
+
+
+if __name__=="__main__":
+
+    root = Tk()
+    app = App(root)
+    root.mainloop()
